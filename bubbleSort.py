25d0b7b4a235a9eaa59e2829a2ff7f3acd0e6b06
Jacob Dent <murkymicrobe88@gmail.com>
2025-01-22 16:19:01 -0500

    Ok I can still do bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,10 +1,30 @@
 
 
+def bubbleSort(inputArray):
 
+    # Copy the array so we are not messing up any data
+    workingArray = inputArray.copy()
+    length = len(inputArray)
+
+    for i in range(length -1):
+        for j in range(length -1 - i):
+            leftN = workingArray[j]
+            rightN = workingArray[j+1]
+
+            if(leftN > rightN):
+                workingArray[j] = rightN
+                workingArray[j+1] = leftN
+
+    return workingArray
 
 
 def main():
-    pass
+    arr1 = [2,3,4,23,9,443,856,343,48,2,1,0,34,65,98,38,23,12]
+
+    arr2 = bubbleSort(arr1)
+
+    print(arr1)
+    print(arr2)
 
 if __name__ == "__main__":
     main()
